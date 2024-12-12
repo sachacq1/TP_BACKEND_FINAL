b06c9b6758998e215e20258abc978327486bff4a
sachacq1 <sachacq1@gmail.com>
2024-12-11 23:56:28 -0300

    change userValidation

--- build/src/validation/userValidation.js
+++ build/src/validation/userValidation.js
@@ -4,11 +4,11 @@ exports.userValidation = void 0;
 const express_validator_1 = require("express-validator");
 const userValidation = () => {
     return [
-        (0, express_validator_1.check)("name").notEmpty().withMessage("El nombre del evento es obligatorio"),
-        (0, express_validator_1.check)("name").isLength({ min: 3 }).withMessage("El nombre del evento debe tener al menos 3 letras"),
-        (0, express_validator_1.check)("name").isLength({ max: 20 }).withMessage("El nombre del evento debe tener menos de 20 letras"),
+        (0, express_validator_1.check)("username").notEmpty().withMessage("El nombre del evento es obligatorio"),
+        (0, express_validator_1.check)("username").isLength({ min: 3 }).withMessage("El nombre del evento debe tener al menos 3 letras"),
+        (0, express_validator_1.check)("username").isLength({ max: 20 }).withMessage("El nombre del evento debe tener menos de 20 letras"),
         (0, express_validator_1.check)("password").notEmpty().withMessage("La contraseña es obligatoria"),
-        (0, express_validator_1.check)("password").isLength({ min: 4 }).withMessage("La contraseña debe tener menos de 4 caracteres"),
+        (0, express_validator_1.check)("password").isLength({ min: 4 }).withMessage("La contraseña debe tener al menos de 4 caracteres"),
         (req, res, next) => {
             const errors = (0, express_validator_1.validationResult)(req);
             if (!errors.isEmpty()) {
--- src/validation/userValidation.ts
+++ src/validation/userValidation.ts
@@ -3,11 +3,11 @@ import { Request, Response, NextFunction } from 'express'
 
 const userValidation = () => {
     return [
-        check("name").notEmpty().withMessage("El nombre del evento es obligatorio"),
-        check("name").isLength({ min: 3 }).withMessage("El nombre del evento debe tener al menos 3 letras"),
-        check("name").isLength({ max: 20 }).withMessage("El nombre del evento debe tener menos de 20 letras"),
+        check("username").notEmpty().withMessage("El nombre del evento es obligatorio"),
+        check("username").isLength({ min: 3 }).withMessage("El nombre del evento debe tener al menos 3 letras"),
+        check("username").isLength({ max: 20 }).withMessage("El nombre del evento debe tener menos de 20 letras"),
         check("password").notEmpty().withMessage("La contraseña es obligatoria"),
-        check("password").isLength({ min: 4 }).withMessage("La contraseña debe tener menos de 4 caracteres"),
+        check("password").isLength({ min: 4 }).withMessage("La contraseña debe tener al menos de 4 caracteres"),
 
 
 
