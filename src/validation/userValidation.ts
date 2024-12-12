3d2db9b1efa71bc59f002eea12e681bd8ca3cede
sachacq1 <sachacq1@gmail.com>
2024-12-11 23:58:09 -0300

    change userValidation

--- src/validation/userValidation.ts
+++ src/validation/userValidation.ts
@@ -3,9 +3,9 @@ import { Request, Response, NextFunction } from 'express'
 
 const userValidation = () => {
     return [
-        check("username").notEmpty().withMessage("El nombre del evento es obligatorio"),
-        check("username").isLength({ min: 3 }).withMessage("El nombre del evento debe tener al menos 3 letras"),
-        check("username").isLength({ max: 20 }).withMessage("El nombre del evento debe tener menos de 20 letras"),
+        check("username").notEmpty().withMessage("El nombre es obligatorio"),
+        check("username").isLength({ min: 3 }).withMessage("El nombre debe tener al menos 3 letras"),
+        check("username").isLength({ max: 20 }).withMessage("El nombre debe tener menos de 20 letras"),
         check("password").notEmpty().withMessage("La contraseña es obligatoria"),
         check("password").isLength({ min: 4 }).withMessage("La contraseña debe tener al menos de 4 caracteres"),
 
